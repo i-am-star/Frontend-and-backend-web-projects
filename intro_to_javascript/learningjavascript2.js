ca6454ced2ff6020e313c293861d62f20eaf121a
uchechi <uchepreciousokekeani@gmail.com>
2022-12-28 18:20:10 +0100

    working on loops.

--- intro_to_javascript/learningjavascript2.js
+++ intro_to_javascript/learningjavascript2.js
@@ -1,6 +1,6 @@
 //looping constructs
 //Using for loop to create a countdown that outputs happy new year.
-for(i=10; i>0; i--){
+ for(i=10; i>0; i--){
     console.log(i)
 }
 console.log("Happy New year"); 
@@ -12,7 +12,7 @@ while (i > 5) {
     console.log(i);
     i--
 }
-console.log("wait a minute");//i is equal to 20. as long as i is greater than 5, minus 1 and repeat till i is equal to 5. and print wait a minute
+console.log("wait a minute");//i is equal to 20. while i is greater than 5, minus 1 and repeat till i is equal to 5. and print wait a minute
 
 //using while loop to count
 var age = 10;
@@ -23,3 +23,9 @@ while(age < 20); {
 console.log("You need to be atleast 18 years old.");
                
  //nested loops
+ for(var i = 2020; i<2025; i++) {
+    console.log(i)
+    for(var k = 1; k < 13; k++) {
+    console.log(k)
+   }
+ }
