9518517da3a5c04aa9be1908e7bc9893c7f05335
i-am-star <adannaya649@gmail.com>
2022-12-29 08:46:39 +0100

    loops and conditionals

--- intro_to_javascript/learningjavascript2.js
+++ intro_to_javascript/learningjavascript2.js
@@ -23,3 +23,46 @@ while(age < 20); {
 console.log("You need to be atleast 18 years old.");
                
  //nested loops
+
+
+//ASSIGNMENT
+
+for(var i=1; i<=10; i++){
+	
+	if(i==1){
+		console.log("Gold medal");
+	}
+else if(i==2){
+	console.log("silver medal");
+	}
+else if(i==3){ 
+	console.log("Bronze medal");
+}
+else {
+	console.log(i,"Level failed");
+}
+
+}
+
+// Using switch instead of if-else
+for(var i=1; i<=10; i++){
+	switch(i){
+
+	case 1: 
+	console.log("Gold medal");
+		break;
+		
+	case 2; 
+	console.log("Silver medal");
+	break;
+	
+	case 3;
+	console.log("Bronze medal");
+	break;
+
+default: 
+	console.log(i, "Level failed");
+
+	}
+}
+
