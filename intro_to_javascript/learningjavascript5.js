0fd18426621c6ff1ae9d4c5946cbfb555af8c6ec
i-am-star <adannaya649@gmail.com>
2023-01-02 15:26:41 +0100

    more variables

---
+++ intro_to_javascript/learningjavascript5.js
@@ -0,0 +1,8 @@
+// declaring variables
+let city = "New york";
+let kfcLocations = 20;  
+{
+console.log("City:" + city + "," + "KFC locations:" + kfcLocations);
+console.log("City:", city "," "KFC locations:", kfcLocations);
+	console.log(`City: &{city}, KFC locations: &{kfcLocations}`);
+}
