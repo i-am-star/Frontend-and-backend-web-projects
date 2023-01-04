2aa9e4a4aa7ec2401cfc281d319f593ae5ea9a67
i-am-star <adannaya649@gmail.com>
2023-01-04 14:28:12 +0100

    Accepting user input.

--- intro_to_javascript/learningjavascript6.js
+++ intro_to_javascript/learningjavascript6.js
@@ -1,6 +1,8 @@
 //Accepting input from users.
 
-let name = prompt("Enter your name:");
-let age = parseint(prompt("Enter your age:"));
+const firstName = prompt("Enter your name:");
+const age = parseInt(prompt("Enter your age:"));
 
-console.log("Hello", name + "you are", age "years old");
+console.log("Hello", firstName,  "\nYou are", age, "years old");
+
+ 
