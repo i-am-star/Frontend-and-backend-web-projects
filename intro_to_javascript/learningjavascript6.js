d61ef4696fdaab5fdd00b2e49d930a5dd55dc1f1
i-am-star <adannaya649@gmail.com>
2023-01-04 15:40:35 +0100

    Accepting user input

--- intro_to_javascript/learningjavascript6.js
+++ intro_to_javascript/learningjavascript6.js
@@ -5,4 +5,13 @@ const age = parseInt(prompt("Enter your age:"));
 
 console.log("Hello", firstName,  "\nYou are", age, "years old");
 
+// program that recieves input from the user and perform arithmetic operations
+
+const x = parseFloat(prompt("Enter a number:"));
+const y = parseFloat(prompt("Enter a number:")); 
+const result = x + y;
+
+console.log(x, "+",  y, "=", result);
+
+
  
